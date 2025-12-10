c6d9d3d16cb7261d3dc95d2a3e2c058049e310d3
thegooda <philipnguyen5050@gmail.com>
2025-12-10 12:45:06 -0800

    -Working on tokens

--- downloader/z1.py
+++ downloader/z1.py
@@ -15,7 +15,7 @@ folder_name = input("Enter folder name: ")
 folder_path = os.path.join(folder_name, "") # Adds correct separator automatically
 
 if "list" in youtube_url:
-    yt_pl = Playlist(youtube_url)
+    yt_pl = Playlist(youtube_url, use_po_token=True)
     print(f"Found {len(yt_pl.video_urls)} videos")
     
     count = 1
@@ -36,7 +36,7 @@ if "list" in youtube_url:
 else:    #if single video 
     # Create a Yotube Object and fetch the stream URL 
     print('Downloading audio from single youtube video...')
-    yt = pytubefix.YouTube(youtube_url)
+    yt = pytubefix.YouTube(youtube_url, use_po_token=True)
     
     # get audio only and download to path given
     print('Fetching audio stream...')
